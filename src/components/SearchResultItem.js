735b049178ee9ff85ae6049a30ad6bdd398a58f2
Derek Cardoso <derek.cardoso@hotmail.com>
2021-12-18 04:21:52 -0300

    style updates

--- src/components/SearchResultItem.js
+++ src/components/SearchResultItem.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import "./SearchResultItem.scss"
 
 function SearchResultItem(props) {
   const {data} = props
 
   console.log('data', data)
   return (
-    <div>
-      <div className="search-result">
-        <div className="left">
-          <img src={data.image.url} alt="bat hero" />
-        </div>
-        <div className="right">
-          <h1>{data.name}</h1>
+    <div className="wrapper">
+      <div className="card">
+        <div className='card__body'>
+        
+          <img src={data.image.url} className="card__image" alt="bat hero" />
+        
+        
+          <h1 className="card__title">{data.name}</h1>
           <span 
-            id="person-name">{data.biography['full-name']}</span>
+            id="person-name">
+            {data.biography['full-name']}
+          </span>
            
-            <div className="stats">
               <div>strength: {data.powerstats.strength}</div>
               <div>speed: {data.powerstats.speed}</div>
               <div>power: {data.powerstats.power}</div>
               
-            </div>
         </div>
       </div>
     </div>
